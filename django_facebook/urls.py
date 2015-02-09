c6b6d411d60870c34ff347fd67bf829d0eb80d36
Tino de Bruijn <tinodb@gmail.com>
2015-02-09 19:56:54 +0100

    Only add /djfb/debug/ view when DEBUG==True

--- django_facebook/urls.py
+++ django_facebook/urls.py
@@ -1,10 +1,15 @@
-from django.conf.urls import *
+from django.conf import settings
+from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('django_facebook.views',
     url(r'^login/$', 'fb_server_login', name='djfb_login'),
     url(r'^login/client/$', 'fb_client_login', name='djfb_clientside_login'),
     url(r'^logout/$', 'fb_logout', name='djfb_logout'),
-    url(r'^debug/$', TemplateView.as_view(template_name='django_facebook/debug.html'),
-        name='djfb_debug'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django_facebook.views',
+        url(r'^debug/$', TemplateView.as_view(
+            template_name='django_facebook/debug.html'), name='djfb_debug'),
+    )
